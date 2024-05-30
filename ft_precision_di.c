cb06ab9569819edca3833cd12339e9417657e3e3
Cloneg7 <151786581+Cloneg7@users.noreply.github.com>
2024-05-30 16:07:37 +0100

    Update ft_precision_di.c

--- ft_precision_di.c
+++ ft_precision_di.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "ft_fprintf.h"
 
 static int	ft_count_nbr(int fd, long int nbr, int *count, int op)
 {
