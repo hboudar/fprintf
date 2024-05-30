f7f861cd6287e7e518dfef3f18195d76bac0c021
Cloneg7 <151786581+Cloneg7@users.noreply.github.com>
2024-05-30 16:07:26 +0100

    Update ft_is_flag.c

--- ft_is_flag.c
+++ ft_is_flag.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "ft_fprintf.h"
 
 static int	ft_is_flag(char c)
 {
